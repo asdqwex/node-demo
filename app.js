5194c362f72936b52c91d5e0db92858475568c07
Matt Ellsworth <matt.ellsworth@asdqwex.net>
2014-11-18 19:48:14 -0600

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var PORT = Number(process.env.PORT || 8000);
+
+http.createServer(function (req, res) {
+  console.log('%d request received', process.pid);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello world!!!!\n!!\nIt works!');
+}).listen(PORT);
+
+console.log('%d listening on %d', process.pid, PORT);
